5fc6934e80197fb2f3094091b064b74114ee7dd9
Satoki Ogiso <ogiso@aclab.esys.tsukuba.ac.jp>
2015-09-10 22:49:53 +0900

    [add] make same path for ruby

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 apt-get update
 apt-get install git -y
+apt-get install vim -y
 
 apt-get install -y apache2
 if ! [ -L /var/www ]; then
@@ -9,3 +10,4 @@ if ! [ -L /var/www ]; then
   ln -fs /vagrant/ieee-towers /var/www
 fi
 apt-get install ruby1.8 -y
+sudo ln -s /usr/bin/ruby /usr/local/bin/ruby
