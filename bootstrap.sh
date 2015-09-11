6e7ce0958fea81e08f6b0a9fd974bbf8827c405b
Satoki Ogiso <ogiso@aclab.esys.tsukuba.ac.jp>
2015-09-11 17:55:49 +0900

    [add] add cgi setup for apache
    insert ExecCGI and AddHandler to enable cgi under /var/www

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,5 +9,13 @@ if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant/ieee-towers /var/www
 fi
+
 apt-get install ruby1.8 -y
-sudo ln -s /usr/bin/ruby /usr/local/bin/ruby
+# Link for ruby binary as if it is in the sakura 
+ln -s /usr/bin/ruby /usr/local/bin/ruby
+
+
+# Enable CGI
+sed -i -e "/..*Directory \/var\/www..*/a\ \t\tAddHandler cgi-script .cgi" /etc/apache2/sites-available/default
+sed -i '/Directory \/var\/www/,/\/Directory/s/Options/Options ExecCGI/' /etc/apache2/sites-available/default
+service apache2 restart
